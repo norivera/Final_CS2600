55c436e36a007b5827b4194cd72dc88337a58b5c
Noe Rivera <Noe>
2021-11-30 08:30:48 +0000

    finshed working on lsh_loop

--- Part1.C
+++ Part1.C
@@ -1,3 +1,26 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+    
+    do{
+        printf(" > ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
 int main(int argc, char **argv)
 {
     //load config files, if any
