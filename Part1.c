431287ae848870ad0843cade8b29ec8496d25aca
Noe Rivera <Noe>
2021-12-03 07:31:15 +0000

    finished working on the shell built in features

--- Part1.c
+++ Part1.c
@@ -4,13 +4,82 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+    Function declarations for builtin shell commands:
+*/
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+    list of built in commands, followed by their corresponding functions
+*/
+char *builtin_str[] = 
+{
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char **) =
+{
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+    builtin function implementations
+*/
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "lsh: epexcted arguments to \"cd\"\n");
+    }
+    else 
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("lsh");
+        }
+    }
+    return -1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Noe Rivera's LSH\n");
+    printf("Type programs names and arguments,and hit enter.\n");
+    printf("The following are built in: \n");
+
+    for (i=0; i <lsh_num_builtins(); i++)
+    {
+        printf (" %s\n", builtin_str[i]);
+    }
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit (char **args)
+{
+    return 0;
+}
+
 void lsh_loop(void)
 {
     char *line;
     char **args;
     int status;
     
-    do{
+    do
+    {
         printf(" > ");
         line = lsh_read_line();
         args = lsh_split_line(line);
@@ -18,7 +87,8 @@ void lsh_loop(void)
 
         free(line);
         free(args);
-    } while (status);
+    } 
+    while (status);
 }
 
 #define LSH_RL_BUFSIZE 1024
