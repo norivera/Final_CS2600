eda4c396bb589580e087013e1eb5ca96919731e6
Noe Rivera <Noe>
2021-12-05 07:34:27 +0000

    Turn off caonical mode and display keypresses

--- kilo.c
+++ kilo.c
@@ -10,16 +10,26 @@ void disableRawMode()
 
 void enableRawMode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 
 int main()
 {
     enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+    if (iscntrl(c))
+    {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
     return 0;
 }
