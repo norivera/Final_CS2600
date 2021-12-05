e6d21d8703d7f2cedacdd3e5a398f8338ddaa84d
Noe Rivera <Noe>
2021-12-05 09:06:28 +0000

    more error handeling

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -14,13 +15,14 @@ void die(const char *s)
 
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode()
 {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  exit(disableRawMode);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
 
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -30,18 +32,18 @@ void enableRawMode()
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
   
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 
 int main()
 {
     enableRawMode();
-    char c;
+
     while (1)
     {
         char c = '\0';
-        read(STDIN_FILENO, &c ,1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
