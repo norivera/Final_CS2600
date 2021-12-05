2438e98e54a3090f979a94c3699ef295230cdf49
Noe Rivera <Noe>
2021-12-05 07:40:32 +0000

    Disable Crtl-S and Ctrl-Q

--- kilo.c
+++ kilo.c
@@ -12,8 +12,10 @@ void disableRawMode()
 void enableRawMode()
 {
   tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
+  exit(disableRawMode);
+
   struct termios raw = orig_termios;
+  raw.c_iflag &= ~(IXON);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
