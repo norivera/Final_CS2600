9849a14ffd5d50736b91f8d894fba3e5d99bc22d
Noe Rivera <Noe>
2021-12-05 08:09:31 +0000

    add a timeout feature for read()

--- kilo.c
+++ kilo.c
@@ -1,6 +1,8 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stdio.h>
 
 struct termios orig_termios;
 
@@ -15,9 +17,13 @@ void enableRawMode()
   exit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -26,14 +32,19 @@ int main()
 {
     enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while (1)
     {
-    if (iscntrl(c))
-    {
-      printf("%d\n", c);
-    } else {
-      printf("%d ('%c')\n", c, c);
+        char c = '\0';
+        read(STDIN_FILENO, &c ,1);
+        if (iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
     }
-  }
     return 0;
 }
