87864adfdf920acfbf623b58fd7db1b79223bfd2
Noe Rivera <Noe>
2021-12-06 06:36:48 +0000

    add clear screen feature

--- kilo.c
+++ kilo.c
@@ -52,6 +52,12 @@ char editorReadKey()
     return c;
 }
 
+/*** output ***/
+void editorRefreshScreen()
+{
+    write (STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 /*** input ***/
 void editorProcessKeypress()
 {
@@ -72,6 +78,7 @@ int main()
 
     while (1)
     {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
     return 0;
