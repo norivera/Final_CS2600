ea31132dd2419882e37bc7a478b8426968a4cb25
Noe Rivera <Noe>
2021-12-07 03:45:43 +0000

    clear lines one at a time

--- kilo.c
+++ kilo.c
@@ -133,6 +133,8 @@ void editorDrawRows(struct abuf *ab)
     for (y = 0; y < E.screenrows; y++)
     {
         abAppend(ab, "~", 1);
+
+        abAppend(ab, "\x1[K", 3);
         if (y < E.screenrows - 1)
         {
             abAppend(ab, "\r\n", 2);
@@ -145,7 +147,6 @@ void editorRefreshScreen()
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1[H", 3);
 
     editorDrawRows(&ab);
