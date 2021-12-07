4b8544f304d26e7e949ae0f452f47eaf38a75d2f
Noe Rivera <Noe>
2021-12-07 20:48:16 +0000

    scrolling with page up and down and move to the end of the line with END

--- kilo.c
+++ kilo.c
@@ -460,39 +460,42 @@ void editorMoveCursor(int key)
     }
 }
 
-void editorProcessKeypress()
-{
-    int c = editorReadKey();
-    switch (c)
-    {
+void editorProcessKeypress() {
+  int c = editorReadKey();
+  switch (c) {
     case CTRL_KEY('q'):
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
-        exit(0);
-        break;
-
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
     case HOME_KEY:
-        E.cx = 0;
-        break;
+      E.cx = 0;
+      break;
     case END_KEY:
-        E.cx = E.screencols - 1;
-        break;
-
+      if (E.cy < E.numrows)
+        E.cx = E.row[E.cy].size;
+      break;
     case PAGE_UP:
     case PAGE_DOWN:
-    {
+      {
+        if (c == PAGE_UP) {
+          E.cy = E.rowoff;
+        } else if (c == PAGE_DOWN) {
+          E.cy = E.rowoff + E.screenrows - 1;
+          if (E.cy > E.numrows) E.cy = E.numrows;
+        }
         int times = E.screenrows;
         while (times--)
-            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
-    }
-    break;
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
     case ARROW_RIGHT:
-        editorMoveCursor(c);
-        break;
-    }
+      editorMoveCursor(c);
+      break;
+  }
 }
 
 /*** init ***/
