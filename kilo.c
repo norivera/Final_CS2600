0cd231b0029fad79b091e0c56ba08229414748a3
Noe Rivera <Noe>
2021-12-08 00:57:35 +0000

    quit confirmation

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@
 /*** defines ***/
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k)&0x1f)
 
@@ -593,6 +594,7 @@ void editorMoveCursor(int key)
 
 void editorProcessKeypress()
 {
+    static int quit_times = KILO_QUIT_TIMES;
     int c = editorReadKey();
     switch (c)
     {
@@ -600,6 +602,14 @@ void editorProcessKeypress()
         /* TODO */
         break;
     case CTRL_KEY('q'):
+        if (E.dirty && quit_times > 0)
+        {
+            editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                                   "Press Ctrl-Q %d more times to quit.",
+                                   quit_times);
+            quit_times--;
+            return;
+        }
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
@@ -650,6 +660,7 @@ void editorProcessKeypress()
         editorInsertChar(c);
         break;
     }
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
